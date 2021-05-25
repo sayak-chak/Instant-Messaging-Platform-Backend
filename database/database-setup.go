4d299ff6e11a1124134b05f4839ba7e7f085cfd7
Sayak Chakraborty <44857480+sayak-chak@users.noreply.github.com>
2021-05-26 02:05:15 +0530

    Extract initial database setup in a package

---
+++ database/database-setup.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"database/sql"
+	"instant-messaging-platform-backend/config"
+)
+
+func SetupDataBase() error {
+	db, err := sql.Open("postgres", config.PostgresConfig)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	_, err = db.Exec("create table if not exists " + config.UsersTable + "(username varchar(20), password varchar(20), PRIMARY KEY (username))")
+	if err != nil {
+		return err
+	}
+	_, err = db.Exec("create table if not exists " + config.CredsTable + "(uuid varchar(36), username varchar(20), PRIMARY KEY (uuid))") //varchar 36 to store uuid
+	if err != nil {
+		return err
+	}
+	_, err = db.Exec("create table if not exists " + config.ChatTable + "(sender varchar(20), room varchar(50), message varchar(500))") 
+	return err
+}
