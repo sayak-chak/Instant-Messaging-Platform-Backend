e871ed9d75c4202f80771610d1c790fd12153b7b
Sayak Chakraborty <44857480+sayak-chak@users.noreply.github.com>
2021-05-26 02:05:38 +0530

    Add mocks for testing

--- mocks/mocks.go
+++ mocks/mocks.go
@@ -1,6 +1,10 @@
 package mocks
 
-import "encoding/json"
+import (
+	"database/sql"
+	"encoding/json"
+	"instant-messaging-platform-backend/config"
+)
 
 func GetLoginJson(username string, password string) ([]byte, error){
 	jsonString := map[string]interface{}{
@@ -8,4 +12,26 @@ func GetLoginJson(username string, password string) ([]byte, error){
 		"password": password,
 	}
 	return json.Marshal(jsonString)
+}
+
+func FillAuthTokenInDB(uuid string, username string) error {
+	db, err := sql.Open("postgres", config.PostgresConfig)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	_, err = db.Exec("insert into " + config.CredsTable + " values ('" + uuid + "','" + username + "')")
+	return err
+}
+
+func RegisterUser(username string) error {
+	db, err := sql.Open("postgres", config.PostgresConfig)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	_, err = db.Exec("insert into " + config.UsersTable + " values ('" + username + "','" + "MOCK_PASSWORD" + "')")
+	return err
 }
